5980b5c5d07188ab7c1845b9fcaf41af28a0a319
Feliks <feliks@Felikss-MacBook-Pro.local>
2026-05-10 16:41:46 +0300

    favorites buttons and navigation logic

--- lib/view/home.dart
+++ lib/view/home.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 
 import '../models/quote.dart';
+import 'favorites.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -26,6 +27,19 @@ class _HomeScreenState extends State<HomeScreen> {
     setState(() {
       _currentQuote = quotes[_random.nextInt(quotes.length)];
     });
+  }
+
+  void _saveToFavorites() {
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(const SnackBar(content: Text('Quote saved to favorites.')));
+  }
+
+  void _openFavorites() {
+    Navigator.push(
+      context,
+      MaterialPageRoute(builder: (context) => const FavoritesScreen()),
+    );
   }
 
   @override
@@ -68,6 +82,24 @@ class _HomeScreenState extends State<HomeScreen> {
                   onPressed: _getNewQuote,
                   icon: const Icon(Icons.refresh),
                   label: const Text('New Quote'),
+                ),
+              ),
+              const SizedBox(height: 12),
+              SizedBox(
+                width: double.infinity,
+                child: FilledButton.icon(
+                  onPressed: _saveToFavorites,
+                  icon: const Icon(Icons.favorite_border),
+                  label: const Text('Save to favorites'),
+                ),
+              ),
+              const SizedBox(height: 12),
+              SizedBox(
+                width: double.infinity,
+                child: TextButton.icon(
+                  onPressed: _openFavorites,
+                  icon: const Icon(Icons.list),
+                  label: const Text('View Favorites'),
                 ),
               ),
             ],
